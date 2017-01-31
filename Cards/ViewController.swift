6368154043a4b00bf016be9da4e6518e2ab3ba1a
almusto <ales.musto@gmail.com>
2017-01-30 19:35:42 -0500

    cards

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,15 +11,44 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Create IB outlets
+  @IBOutlet weak var topLeftSuit: UILabel!
+  @IBOutlet weak var bottomRightSuit: UILabel!
+  @IBOutlet weak var cardNumber: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      topLeftSuit.text = ""
+      bottomRightSuit.text = ""
+      cardNumber.text = ""
     }
     
     
     
     // TODO: IB actions and code to update UI
 
-  
+  @IBAction func fourClubs(_ sender: Any) {
+    topLeftSuit.text = "♣️"
+    bottomRightSuit.text = "♣️"
+    cardNumber.text = "4"
 
+  }
+  
+  @IBAction func threeSpades(_ sender: Any) {
+    topLeftSuit.text = "♠️"
+    bottomRightSuit.text = "♠️"
+    cardNumber.text = "3"
+
+  }
+
+  @IBAction func eightDimonds(_ sender: Any) {
+    topLeftSuit.text = "♦️"
+    bottomRightSuit.text = "♦️"
+    cardNumber.text = "8"
+  }
+
+  @IBAction func tenHearts(_ sender: Any) {
+    topLeftSuit.text = "♥️"
+    bottomRightSuit.text = "♥️"
+    cardNumber.text = "10"
+  }
 }
